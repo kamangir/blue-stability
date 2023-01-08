9376e365abfec07be7a6b1eab6dce2e4c3023205
kamangir <arash@kamangir.net>
2023-01-07 20:27:07 -0800

    validate += dryrun - kamangir/bolt#741

--- .abcli/validate.sh
+++ .abcli/validate.sh
@@ -5,24 +5,25 @@ function blue_stability_generate_validate() {
     local app_name=$(abcli_option "$options" app blue_stability)
 
     if [ $(abcli_option_int "$options" help 0) == 1 ] ; then
-        abcli_show_usage "$app_name generate validate$ABCUL[app=<app-name>,what=all|image|video]" \
+        abcli_show_usage "$app_name generate validate$ABCUL[app=<app-name>,dryrun,what=all|image|video]" \
             "validate $app_name."
         return
     fi
 
+    local dryrun=$(abcli_option_int "$options" dryrun 0)
     local what=$(abcli_option "$options" what all)
 
     if [ "$what" == "all" ] ; then
         blue_stability_generate_validate \
-            app=$app_name,what=image
+            app=$app_name,dryrun=$dryrun,what=image
         blue_stability_generate_validate \
-            app=$app_name,what=video
+            app=$app_name,dryrun=$dryrun,what=video
         return
     fi
 
     if [ "$what" == "image" ] ; then
         blue_stability generate image \
-            app=$app_name,~dryrun \
+            app=$app_name,dryrun=$dryrun \
             validation - \
             "an orange carrot walking on Mars." \
             --seed 42
@@ -31,7 +32,7 @@ function blue_stability_generate_validate() {
 
     if [ "$what" == "video" ] ; then
         blue_stability generate video \
-            app=$app_name,~dryrun,frame_count=3,marker=PART,url \
+            app=$app_name,dryrun=$dryrun,frame_count=3,marker=PART,url \
             https://www.gutenberg.org/cache/epub/51833/pg51833.txt \
             --seed 42 \
             --start_schedule 0.9
