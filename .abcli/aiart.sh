528c0e2fc5d9cddd8fc618bde03122db18083bae
kamangir <arash@kamangir.net>
2023-01-10 22:06:10 -0800

    aiart clean-up - kamangir/bolt#741

--- .abcli/aiart.sh
+++ .abcli/aiart.sh
@@ -4,7 +4,7 @@ function blue_stability_generate() {
     aiart_generate \
         "$1" \
         $(abcli_option_default "$2" app blue_stability) \
-        ${@:3}
+        "${@:3}"
 }
 
 function blue_stability_transform() {
--- blue_stability/__init__.py
+++ blue_stability/__init__.py
@@ -1,3 +1,3 @@
 NAME = "blue_stability"
 
-VERSION = "4.1.1"
+VERSION = "4.2.1"
